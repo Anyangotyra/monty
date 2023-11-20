10fe09646671b40138b18849b05828c9d71ae41c
OduogoWalter <oduogowalter@gmail.com>
2023-11-19 22:59:15 -0800

    push.c

---
+++ push.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+/**
+ * push - Pushes an element onto the stack.
+ * @stack: A pointer to the top of the stack.
+ * @line_number: The line number in the Monty bytecode file.
+ *
+ * Description:
+ * The push function adds a new element to the top of the stack.
+ * It takes an integer argument from the Monty bytecode file and
+ * creates a new node with that value, pushing it onto the stack.
+ *
+ * @stack: A pointer to the top of the stack.
+ * @line_number: The line number in the Monty bytecode file where
+ * the push operation is executed.
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+	/* Implementation of push goes here */
+}
